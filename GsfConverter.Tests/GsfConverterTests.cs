2a8ba039e0a0bf77abebaa879901eb4cfa04c398
Kay Altos <kayaltosnexus@gmail.com>
2022-10-17 19:24:24 +0300

    Добавьте файлы проекта.

---
+++ GsfConverter.Tests/GsfConverterTests.cs
@@ -0,0 +1,47 @@
+namespace GsfConverter.Tests
+{
+    [TestClass]
+    public class GsfConverterTests
+    {
+        [TestMethod]
+        public void ReadFile_PointsList()
+        {
+            // arrange
+            var path = "Accreation_ASCII_1.rawst";
+            var expected = new List<(double X, double Y, double Z)>();
+
+            // act
+            var actualList = Converter.ReadStructure(path);
+
+            // assert
+            Assert.AreEqual(expected.GetType(), actualList.GetType());
+        }
+
+        [TestMethod]
+        public void ADConvert_DiscretePointsList()
+        {
+            // arrange
+            var path = "Accreation_ASCII_1.rawst";
+            var expected = new List<(int X, int Y, int Z)>();
+
+            // act
+            var actualList = Converter.ADConvert(Converter.ReadStructure(path));              
+
+            // assert
+            Assert.AreEqual(expected.GetType(), actualList.GetType());
+        }
+        [TestMethod]
+        public void SaveStructure_GsfFile()
+        {
+            // arrange
+            var path = "Accreation_ASCII_1.rawst";
+            var pathGsf = "NoseTest.gsf";
+            // act
+            var gsfList = Converter.ADConvert(Converter.ReadStructure(path));
+            Converter.SaveToGsf(pathGsf, gsfList);
+
+            // assert
+            Assert.IsTrue(File.Exists(pathGsf));
+        }
+    }
+}
---
+++ GsfConverter/Converter.cs
@@ -0,0 +1,56 @@
+﻿using System.Globalization;
+
+namespace GsfConverter;
+
+public static class Converter
+{
+    public static List<(double X, double Y, double Z)> ReadStructure (string path)
+    {
+        var pointsList = new List<(double X, double Y, double Z)> ();
+        string[] lines = File.ReadAllLines(path);
+        for (int i = 1; i < lines.Length; i++)
+        {
+            var line = lines[i].Split(", ");
+            pointsList.Add((float.Parse(
+                line[1], new CultureInfo("en-us")), 
+                float.Parse(line[2], new CultureInfo("en-us")), 
+                float.Parse(line[3], new CultureInfo("en-us")
+                )
+                ));
+        }
+        return pointsList;
+    }
+    
+    public static List<(int X, int Y, int Z)> ADConvert (
+        List<(double X, double Y, double Z)> points, 
+        int multiplier
+        )
+    {
+        int discretizationCoefficient = multiplier;
+       
+        List<(int X, int Y, int Z)> discretePoints = new List<(int X, int Y, int Z)>();
+        
+        foreach (var point in points)
+        {
+            int X = (int)(point.X * discretizationCoefficient);
+            int Y = (int)(point.Y * discretizationCoefficient);
+            int Z = (int)(point.Z * discretizationCoefficient);                
+            discretePoints.Add((X, Y, Z));
+        }           
+        return discretePoints;
+    }
+
+    public static void SaveToGsf(string path, List<(int X, int Y, int Z)> points)
+    {
+        StreamWriter sw = new StreamWriter(path);
+        
+        sw.WriteLine("Structure");
+        foreach (var p in points)
+        {
+            sw.WriteLine(string.Join("\t", new string[] {p.X.ToString(),p.Y.ToString(),
+                                p.Z.ToString(), "1"}));
+        }
+                    
+        sw.Close();
+    }
+}
